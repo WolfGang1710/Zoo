76c65069feb786b9bd0455c8f6ea30f957fc8d39
William ROGUET <william.roguet@gmail.com>
2024-01-10 12:03:29 +0100

    Implémentation class `Zoo`, function addAnimal

--- Zoo.cpp
+++ Zoo.cpp
@@ -4,11 +4,16 @@
 
 #include "headers/Zoo.h"
 
-Zoo::Zoo(const string &_name) : name(_name) {}
+Zoo::Zoo() : name(""), size(0) {}
+
+Zoo::Zoo(const string &_name) : name(_name) {
+    size = 0;
+}
 
 Zoo::Zoo(Zoo &other) {
     name = other.name;
     animals = other.animals;
+    size=other.size;
 }
 
 const string &Zoo::getName() const {
@@ -17,4 +22,21 @@ const string &Zoo::getName() const {
 
 void Zoo::setName(const string &_name) {
     Zoo::name = _name;
+}
+
+void Zoo::addAnimal(Animal newAnimal) {
+    /*
+     * Attention :
+     * - le zoo ne doit pas être plein
+     * -deux animaux ne peuvent avoir un même nom dans le Zoo
+     */
+    if(size >= MAX_CAPACITY){
+        cout << endl << "Erreur : le Zoo est déjà plein !";
+    } else {
+        if(searchAnimalByName(newAnimal.getName())==-1) {
+            cout << endl << "Erreur : un animal possède déjà ce nom dans le Zoo";
+        } else {
+            lower_bound() //https://en.cppreference.com/w/cpp/algorithm/lower_bound
+        }
+    }
 }
--- headers/Zoo.h
+++ headers/Zoo.h
@@ -13,15 +13,17 @@ class Zoo {
 private:
     string name; // Nom du Zoo
     static int MAX_CAPACITY; // la capacite maximale du Zoo
+    int size;
     vector<Animal*> animals; // Vecteur d'animaux de la class `Animal`.
 public:
-    Zoo() = default;
-    explicit Zoo(const string &name);
+    Zoo();
+    Zoo(const string &name);
     Zoo(Zoo &other);
 
     const string &getName() const;
     void setName(const string &name);
 
+    void addAnimal(Animal newAnimal);
 
 };
 
