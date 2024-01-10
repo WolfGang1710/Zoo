812002a595d2e733eb4dbc84829a0c66c22064bc
William ROGUET <william.roguet@gmail.com>
2024-01-10 10:41:09 +0100

    Implémentation class `Animal`

---
+++ Animal.cpp
@@ -0,0 +1,111 @@
+//
+// Created by ecole on 10/01/2024.
+//
+
+#include "headers/Animal.h"
+
+Animal::Animal(string &_name, string &_color, string &_diet, string &_habitat, string &_sounds, bool _isPet, int _age,
+               double _weight, double _height):
+               name(_name), color(_color), diet(_diet), habitat(_habitat), sounds(_sounds),
+               isPet(_isPet),
+               age(_age), weight(_weight), height(_height)
+{
+}
+
+const string &Animal::getName() const {
+    return name;
+}
+
+void Animal::setName(const string &_name) {
+    Animal::name = _name;
+}
+
+const string &Animal::getColor() const {
+    return color;
+}
+
+void Animal::setColor(const string &_color) {
+    Animal::color = _color;
+}
+
+const string &Animal::getDiet() const {
+    return diet;
+}
+
+void Animal::setDiet(const string &_diet) {
+    Animal::diet = _diet;
+}
+
+const string &Animal::getHabitat() const {
+    return habitat;
+}
+
+void Animal::setHabitat(const string &_habitat) {
+    Animal::habitat = _habitat;
+}
+
+const string &Animal::getSounds() const {
+    return sounds;
+}
+
+void Animal::setSounds(const string &_sounds) {
+    Animal::sounds = _sounds;
+}
+
+bool Animal::isPetFunc() const {
+    return isPet;
+}
+
+void Animal::setIsPet(bool _isPet) {
+    Animal::isPet = _isPet;
+}
+
+int Animal::getAge() const {
+    return age;
+}
+
+void Animal::setAge(int _age) {
+    Animal::age = _age;
+}
+
+double Animal::getWeight() const {
+    return weight;
+}
+
+void Animal::setWeight(double _weight) {
+    Animal::weight = _weight;
+}
+
+double Animal::getHeight() const {
+    return height;
+}
+
+void Animal::setHeight(double _height) {
+    Animal::height = _height;
+}
+
+void Animal::printInfo() {
+    string name = Animal::getName();
+    string color = Animal::getColor();
+    string diet = Animal::getDiet();
+    string habitat = Animal::getHabitat();
+    // string sounds = Animal::getSounds(); // not used now
+    bool isPet = Animal::isPetFunc();
+    int age = Animal::getAge();
+    double weight = Animal::getWeight();
+    double heigth = Animal::getHeight();
+
+    cout << endl << "Animal of unknown type:";
+    cout << endl << "Name : " << name;
+    cout << endl << "Color : " << color;
+    cout << endl << "Diet" << diet;
+    cout << endl << "Habitat" << habitat;
+    cout << endl << "Age : " << age << "'s old";
+    if(isPet){
+        cout << endl << "This animal is a pet";
+    } else {
+        cout << endl << "This animal is not a pet";
+    }
+    cout << endl << "Weight : " << weight;
+    cout << endl << "Height : " << height;
+}
---
+++ headers/Animal.h
@@ -0,0 +1,61 @@
+//
+// Created by ecole on 10/01/2024.
+//
+
+#ifndef ZOO_ANIMAL_H
+#define ZOO_ANIMAL_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Animal {
+private:
+    string name; // nom de l'animal
+    string color; // couleur de l'animal
+    string diet; // regime alimentaire de l'animal
+    string habitat; // habitat de l'animal
+    string sounds; // son de l'animal
+    bool isPet; // si l'animal est considéré comme animal de compagnie
+    int age; // son age
+    double weight; // son poids
+    double height; // sa taille
+public:
+    Animal()=default;
+    Animal(string &name, string &color, string &diet, string &habitat, string &sounds,
+           bool isPet, int age, double weight, double height);
+    //Animal() // Constructeur de copie
+
+    const string &getName() const;
+    void setName(const string &name);
+
+    const string &getColor() const;
+    void setColor(const string &color);
+
+    const string &getDiet() const;
+    void setDiet(const string &diet);
+
+    const string &getHabitat() const;
+    void setHabitat(const string &habitat);
+
+    const string &getSounds() const;
+    void setSounds(const string &sounds);
+
+    bool isPetFunc() const;
+    void setIsPet(bool isPet);
+
+    int getAge() const;
+    void setAge(int age);
+
+    double getWeight() const;
+    void setWeight(double weight);
+
+    double getHeight() const;
+    void setHeight(double height);
+
+    virtual void printInfo();
+};
+
+
+#endif //ZOO_ANIMAL_H
